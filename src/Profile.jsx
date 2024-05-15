f0e4201d460d6c178b2729df708af60303292709
chocoboash <anleal2004@gmail.com>
2024-05-14 22:37:35 -0400

    Display members information

---
+++ src/Profile.jsx
@@ -0,0 +1,9 @@
+export default function Profile(props) {
+    return (
+        <div className="Profile">
+            <h1>{props.name}</h1>
+            <h2>{props.position}</h2>
+            <img src={props.picture} alt={props.name} />
+        </div>
+    );
+}
