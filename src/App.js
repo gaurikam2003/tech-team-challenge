a1454ac373d63ec4f1fdc332f2d3bc5a86dde6c7
asmita-chandra <105055090+asmita-chandra@users.noreply.github.com>
2024-05-23 17:47:09 +0000

    Made changes to the page using App.js and App.css also added a background photo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import data from './members_details.json'
+import members from './members_details.json';
 
 function App() {
+
+  //to iterate through member cards (show 4 at a time)
+  const [startIndex, setStartIndex] = useState(0);
+  const membersToShow = members.slice(startIndex, startIndex + 4);
+
+  //to execute when the next button is clicked
+  const handleNextClick = () => {
+    if (startIndex === 3) {
+      setStartIndex(prevIndex => Math.max(0, prevIndex - 3));
+    } else {
+      setStartIndex(prevIndex => Math.max(0, prevIndex + 3));
+    }
+  };
+
+  //to execute when the previous button is clicked
+  const handlePrevClick = () => {
+    if (startIndex === 0) {
+      setStartIndex(prevIndex => Math.max(0, prevIndex + 3));
+    } else {
+      setStartIndex(prevIndex => Math.max(0, prevIndex - 3));
+    }
+  };
+
+
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>F!rosh Orientation Committee</h1>
+      </header>
+      <div className='mainpage'>
+        <div className="arrows">
+          <button className="arrow-btn left-arrow" onClick={handlePrevClick}>
+            &lt;
+          </button>
+          <div className="members-container">
+            <div className="member-cards">
+              {membersToShow.map((member, index) => (
+                <div key={index} className="member-card">
+                  <img src={member.picture} alt={member.name} className="member-photo" />
+                  <h2>{member.name}</h2>
+                  <p>{member.position}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <button className="arrow-btn right-arrow" onClick={handleNextClick}>
+            &gt;
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
