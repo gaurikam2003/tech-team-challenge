78fed6db5f9d4751bcf83e058e4dfc2e85e8b9b7
Jiahan William Wen <wenjiahan2002@gmail.com>
2024-05-23 14:45:03 -0400

    William's Design Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-import data from './members_details.json'
+// import data from './members_details.json'
+import Members from './Members';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Members />
     </div>
   );
 }
