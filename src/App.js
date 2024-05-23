9a4c5805b1d4720b5b31b001ee76e03631d5fe61
alissaxx <xiang.kx@outlook.com>
2024-05-23 16:14:12 -0400

    Alissa Xiang Application Submission

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
-import data from './members_details.json'
+import data from './members_details.json';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Team">
+      <h1>F!rosh Orientation Committee</h1>
+      <div className="team-list">
+        {data.map((teamMember) => (
+          <div key={teamMember.name} className="team-card">
+            <img src={teamMember.picture} alt={teamMember.name} className="team-photo" />
+            <h3 className="team-name">{teamMember.name}</h3>
+            <p className="team-role">{teamMember.position}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
