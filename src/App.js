3de296b162aea01057c67d8bba34c02ff258de3a
Sunny <78620585+sunnwydays@users.noreply.github.com>
2024-05-24 00:00:31 -0400

    Add files via upload
    # Applicant Information
    
    ### Name:
    Sunny Wu
    
    ### Email:
    sun.wu@mail.utoronto.ca
    
    ### Solution description:
    Welcomed the user and added cards for everyone!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 import data from './members_details.json'
+import Card from './Card'
 
 function App() {
   return (
     <div className="App">
+      <h1>Welcome to FROSH!!!!</h1>
+      <h2 className="h2">Meet the team</h2>
+      {data.map((member, index) => (
+        <Card
+          key={index}
+          name={member.name}
+          position={member.position}
+          picture={member.picture}
+        />
+      ))}
     </div>
   );
 }
