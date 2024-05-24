279fb105915bfa6a40cce6359a04a147cc70f69c
George Fan <78384472+Wozunubi@users.noreply.github.com>
2024-05-23 23:14:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import data from './members_details.json'
 function App() {
   return (
     <div className="App">
+      <ul>
+        {data.map((member, index) => (
+          <li key={index}>
+            <h1>{member.name}</h1>
+            <h2>{member.position}</h2>
+            <img src={member.picture} alt={member.name} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
