04c7eb613f7b1f2b4c12891a03271ca0a360bcdd
bruhspicy <geraldyywang@gmail.com>
2024-05-25 13:09:00 -0400

    submission

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
-import data from './members_details.json'
+import React from "react";
+import "./App.css";
+import data from "./members_details.json";
+
+import MemberCard from "./components/MemberCard";
 
 function App() {
   return (
-    <div className="App">
+    <div className=" flex justify-center items-center w-full">
+      <div className=" flex-col m-10">
+        <h1 className=" font-bold text-purple-700 text-4xl rounded-lg p-4 w-1/2 text-center bg-purple-100">
+          Meet the Orientation Committee!
+        </h1>
+        <div className="flex flex-wrap items-center justify-center h-1/4 bg-gray-100 rounded-lg mt-4">
+          {data.map((member) => (
+            <MemberCard member={member} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
